1522f9ff6a199495506278ceb845801d046d2198
Greg Kapka <greg@kapka.co.uk>
2018-05-03 00:50:02 +0100

    MVP - v quick and v dirty!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BlockchainContext, BlockchainProvider } from './context/blockchain'
 
 const App = () => (
-  <p>Hello!</p>
+  <BlockchainProvider>
+    <BlockchainContext.Consumer>
+      {({ blockchain, blockNum, submitTransaction, mineBlock }) => {
+        let x = () => console.log('The blockchain: ', blockchain)
+        return (
+          <React.Fragment>
+            <p>Block number: {blockNum}</p>
+            <button onClick={() => submitTransaction(['Winslow', 'Homer'])}>Add TX</button><br/><br/>
+            <button onClick={() => mineBlock()}>Mine Block</button><br/><br/>
+            <button onClick={x}>Console Log BC</button>
+          </React.Fragment>
+      )}}
+    </BlockchainContext.Consumer>
+  </BlockchainProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'));
