e7cc630025f9819abbf062eaf6b9924a34c7267c
Greg Kapka <greg@kapka.co.uk>
2018-05-03 00:57:49 +0100

    add comments

--- src/context/blockchain.js
+++ src/context/blockchain.js
@@ -24,7 +24,9 @@ export class BlockchainProvider extends React.Component {
   componentDidMount() {
     this.createBlock() // Genesis block...
   }
-
+  /**
+   * Create block prototype
+   */
   createBlock(index = 0, prevHash = '00000000', hash = 0, nonce = 0, txs = []) {
     this.setState({
       currentBlock: {
@@ -37,33 +39,48 @@ export class BlockchainProvider extends React.Component {
       }
     })
   }
-
-  getKey(_txs, _prevHash, _blockIndex, _nonce) {
+  /**
+   * Returns key string to be hashed with the nonce
+   */
+  getKey(_txs, _prevHash, _blockIndex) {
     return JSON.stringify(_txs) + _prevHash + _blockIndex
   }
-
+  /**
+   * Performs proof of work on the current block's txs before adding
+   * it to bc.
+   */
   mineBlock() {
     this.addToBlockchain(this.generateHash(this.getCurrentBlock()))
   }
-
+  /**
+   * Adds results of PoW algo to current block, appends it to bc 
+   * and generates the next empty block with correct deets as 
+   * the new "current block"
+   */
   addToBlockchain({ hash, nonce }) {
     let chain = this.state.blockchain
     let block = this.getCurrentBlock()
-    this.createBlock(chain.length + 1, hash)
     block.nonce = nonce
     block.hash = hash
     chain.push(block)
+    this.createBlock(chain.length + 1, hash)
     this.setState({blockchain: chain})
   }
-
-
+  /**
+   * Chucks a "tx" in the tx array in the current block
+   */
   submitTransaction(_tx) {
     let block = this.getCurrentBlock()
     block.txs.push(_tx)
     this.setState({currentBlock: block})
     console.log('Current Block: ', this.state.currentBlock)
   }
-
+  /**
+   * Keccack hashes current blocks txs with the block key until 
+   * hash prefixed with correct number of 0's is discovered, per
+   * Nakomoto's original algo. Returns that hash plus the nonce 
+   * that generated it.
+   */
   generateHash(_block) {
     let nonce = _block.nonce
       , hash = keccak('keccak256').update(_block.key + nonce).digest('hex')
@@ -75,8 +92,6 @@ export class BlockchainProvider extends React.Component {
     return { hash, nonce }
   }
   
-
-
   getCurrentBlock() {
     return this.state.currentBlock
   }
